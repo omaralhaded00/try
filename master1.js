f3b350f23520cee61243671a2155ea8b8e6defa1
omar-alhaded <66205421+omar-alhaded@users.noreply.github.com>
2020-12-11 15:20:44 +0200

    Create master1.js

---
+++ master1.js
@@ -0,0 +1,47 @@
+
+$('.hand.nosel.fl.uzr.border')['css']({ margin: '1px 0', borderRadius: '0px' }), $('.fa-gear')['text'](' الضبط')['css']('width', '');
+
+
+$(`<img src="https://chatnsh.com/sico/z1ep8tg2c710.jpg" class="fr" style="width:100%;margin-right: 1px;"> 
+    <div>
+     <marquee direction="right" width="99%" id="asim7" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="2">
+       ·● اهلا وسهلا بكم في شات آحـــبك للجوال | هـنـا تـكون فرفشه وسعاده وتقضيت الوقت معى اطيب الناس ●· 
+     </marquee>
+    </div> 
+    <div id="asim10">
+     <a id="asim21" href="#
+" target="#" type="button" style=" width: 24%; margin: 2px;" class="btn-as">اتصل بنا</a> 
+     <a id="asim21" href="mobile.html" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">التطبيق</a> 
+     <a id="asim21" href="" target="#" type="button" style=" width: 24%; margin: 2px;" class="btn-as">وظائف شاغره</a> 
+     <a id="asim21" href="#" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">اختصارات</a>
+     <a id="asim22" href="#" target="1" type="1" style=" width: 24%; margin: 2px;" class="btn-as">اشتراكات</a> 
+     <a id="asim22" href="#" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">زخرفة</a> 
+     <a id="asim22" href="#" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">ق.السوبر</a> 
+     <a id="asim22" href="#" target="#" type="button" style=" width: 24%; margin: 2px;" class="btn-as">الأبراج</a>
+    </div>
+`).insertBefore('.nav-tabs');
+$('.nav-tabs')['addClass']('fl')['css']('width', '100%')
+var nopm = !0
+    , nonot = !0;
+$('.hand.nosel.fl.uzr.border')['css']({margin: '1px 0', borderRadius: '0px'})
+$(`<link rel="stylesheet" href="https://rawcdn.githack.com/omar-alhaded/try/ef18c74cfb3c791aed34738b1f0f4c1695a38aaf/master1.css">`).insertBefore('body');
+$(".fa-gear").text("الضبط ").css("width","");
+$("[data-target=#wall]").attr('title',' متنفس الآقلأم و القلوب ')
+$("[data-target=#settings]").attr('title','     الأعدادات و الضبط')
+$("[data-target=#rooms]").attr('title','   الغرف و الرومـات ')
+$("[data-target=#chats]").attr('title','      المحادثات الخاصه  ')
+$("[data-target=#users]").attr('title','  ضيـوفنـا المتـواجدين  ')
+$('.fr.borderg a').text('عمر الحديد')
+$(".ubnr").remove();
+$('.fr.borderg a')['text']('جوال هوست');
+$('.fa-commenting-o')['text']('  حائط ')['css']('width', '');
+$('a.fa.fa-user-plus')['text']('تسجيل');
+$('a.fa.fa-user ')['text']('دخـول');
+$(`<label onclick="window.open('cp?token='+token,'_blank');" style="background-color: ghostwhite;color: black;margin:4px; padding:8px;width:98%;" class="label tc border cp  btn  label-danger fl"><span class="fl fa fa-star"></span>ادارة بيانات المواقع</label>`).insertAfter('#settings label.label.tc.border.btn.fl[onclick="sendpic();"]');
+$(`<label onclick="window.open('قريبا تحت تطوير');" style="background-color: ghostwhite;color: black;margin:4px; padding:8px;width:98%;" class="label tc border cp  btn  label-danger fl"><span class="fl fa fa-user-times"></span>ادارة الوهميين</label>`).insertAfter('#settings label.label.tc.border.btn.fl[onclick="sendpic();"]');
+$('.u-msg.dots').removeClass('dots');
+Tsend_=Tsend;reqlikes=5;Tsend=function(){if(getuser(myid).rep<reqlikes){alert('لإ يمكنك الكتابة بالعام يتطلب منك الحصول على لايكات'+reqlikes+' ');return;}else{Tsend_();}}
+sendpic_=sendpic;reqlikes=5;sendpic=function(){if(getuser(myid).rep<reqlikes){alert('لإ يمكنك تغير صورتك يتطلب منك الحصول على لايكات '+reqlikes+'');return;}else{sendpic_();}}
+setprofile_=setprofile;reqlikes=5;setprofile=function(){if(getuser(myid).rep<reqlikes){alert('لا يمكنك حفظ الأعدادات يتطلب منك الحصول على لايكات '+reqlikes+'');return;}else{setprofile_();}}
+
+
